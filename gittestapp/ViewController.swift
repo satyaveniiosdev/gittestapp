e53d2c9380948701e04bf31fb6034fc75c76adde
satyaveni <satyaveni.iosdev@gmail.com>
2020-05-09 16:20:51 +0530

    hi

---
+++ gittestapp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gittestapp
+//
+//  Created by SwaSa on 09/05/20.
+//  Copyright © 2020 SwaSa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hello world")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
